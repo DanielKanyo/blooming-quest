9e3a3b1e0f4f9bf70da79ea0de3f5b9ce0464bed
danielkanyo <danielkanyo@debian.com>
2024-06-27 16:43:17 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
+import { HomePage } from "./pages/Home.page";
+
+export default function App() {
+    return (
+        <MantineProvider theme={theme}>
+            <HomePage />
+        </MantineProvider>
+    );
+}
