08076ceb91eb6c5a6ddae34c29449d075211c3da
DanielKanyo <danielkanyog@gmail.com>
2024-07-16 13:08:31 +0200

    House area redesign

--- src/Layouts/HouseArea/HouseArea.tsx
+++ src/Layouts/HouseArea/HouseArea.tsx
@@ -1,23 +1,19 @@
-import { Card, Center, Image } from "@mantine/core";
+import { Card, Image } from "@mantine/core";
 
-import house from "../../Assets/Other/house.png";
+import farm from "../../Assets/Other/farm.png";
 import "./HouseArea.css";
 
 export function HouseArea() {
     return (
         <Card className="house-area" shadow="sm" padding="0" radius="sm" h={190} mb={10}>
             <div style={{ overflow: "hidden", height: "100%", position: "relative" }}>
-                <div className="sky"></div>
-                <div className="sun"></div>
-                <div className="cloud"></div>
                 <div className="land"></div>
-                <Center h="100%">
-                    <Image src={house} h={110} w={110} style={{ zIndex: 1 }}></Image>
-                </Center>
-                <div className="tree1"></div>
-                <div className="tree2"></div>
-                <div className="bush"></div>
-                <div className="fence"></div>
+                <div className="cloud"></div>
+                <div className="cloud2"></div>
+                <div className="tree"></div>
+                <Image className="barn" src={farm} h={140} w={140}></Image>
+                <div className="tractor"></div>
+                <div className="hay"></div>
             </div>
         </Card>
     );
