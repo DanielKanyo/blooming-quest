92f4008d854db5a0e43fd5b2eacfd50f542e035c
DanielKanyo <danielkanyog@gmail.com>
2024-07-18 08:46:23 +0200

    Optimizing QuestItem component

--- src/Components/QuestItem.tsx
+++ src/Components/QuestItem.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Accordion, Text, Group, Avatar, Blockquote, ActionIcon, Tooltip, Alert, Flex, HoverCard, Image, Center } from "@mantine/core";
@@ -26,6 +26,8 @@ import { addQuestToChallenge, completeQuestInChallenge } from "../Store/Features
 import store from "../Store/Store";
 import { BadgeWithImage } from "./BadgeWithImage/BadgeWithImage";
 
+const GOLD_COLOR = "#FFD700";
+
 type QuestItemProps = {
     quest: Quest;
     challenge: Challenge;
@@ -37,9 +39,17 @@ interface AccordionLabelProps {
     category: QuestCategories;
 }
 
-const GOLD_COLOR = "#FFD700";
+interface QuestActions {
+    quest: Quest;
+    challenge: Challenge;
+    acceptMode: boolean;
+    handleAccept: () => void;
+    handleComplete: () => void;
+    handleRemove: () => void;
+    loading: boolean;
+}
 
-function AccordionLabel({ description, category }: AccordionLabelProps) {
+const AccordionLabel = ({ description, category }: AccordionLabelProps) => {
     const Icon = CategoryIconMapping.get(category) || IconQuestionMark;
 
     return (
@@ -57,73 +67,132 @@ function AccordionLabel({ description, category }: AccordionLabelProps) {
             </div>
         </Group>
     );
-}
+};
+
+const getDifficultyIcon = (difficulty: QuestDifficulties): string => {
+    switch (difficulty) {
+        case QuestDifficulties.Hard:
+            return flame;
+        case QuestDifficulties.Medium:
+            return seaWave;
+        case QuestDifficulties.Easy:
+            return wind;
+        default:
+            throw new Error("Unsupported quest difficulty...");
+    }
+};
+
+const ExtraRewardHoverCard = ({ extraReward }: { extraReward: string }) => {
+    return (
+        <HoverCard shadow="md">
+            <HoverCard.Target>
+                <Center>
+                    <BadgeWithImage imgSrc={EXTRA_REWARDS.get(extraReward)!} text="1x" color={GOLD_COLOR} shine={true} />
+                </Center>
+            </HoverCard.Target>
+            <HoverCard.Dropdown>
+                <Image h={46} w={46} src={EXTRA_REWARDS.get(extraReward)!} />
+            </HoverCard.Dropdown>
+        </HoverCard>
+    );
+};
+
+const QuestActions = ({ quest, challenge, acceptMode, handleAccept, handleComplete, handleRemove, loading }: QuestActions) => {
+    const user = useSelector((state: ReturnType<typeof store.getState>) => state.user);
+
+    return acceptMode ? (
+        <Flex>
+            {user.roles.includes(UserRoles.ADMINISTRATOR) && (
+                <Tooltip label="Delete Quest" position="bottom" color="gray">
+                    <ActionIcon
+                        mr={8}
+                        w="100%"
+                        variant="light"
+                        color="red"
+                        aria-label="delete-quest"
+                        onClick={handleRemove}
+                        disabled={loading}
+                    >
+                        <IconX size={18} />
+                    </ActionIcon>
+                </Tooltip>
+            )}
+            <Tooltip label="Accept Quest" position="bottom" color="gray">
+                <ActionIcon
+                    variant="light"
+                    w="100%"
+                    color={CategoryColorMapping.get(quest.category)}
+                    disabled={loading}
+                    aria-label="accept-quest"
+                    onClick={handleAccept}
+                >
+                    <IconPlus size={18} />
+                </ActionIcon>
+            </Tooltip>
+        </Flex>
+    ) : (
+        !challenge.completedQuests.includes(quest.id) && (
+            <Tooltip label="Complete Quest" position="bottom" color="gray">
+                <ActionIcon
+                    variant="light"
+                    w="100%"
+                    color={CategoryColorMapping.get(quest.category)}
+                    aria-label="complete-quest"
+                    disabled={loading}
+                    onClick={handleComplete}
+                >
+                    <IconCheck size={18} />
+                </ActionIcon>
+            </Tooltip>
+        )
+    );
+};
 
 export function QuestItem({ quest, challenge, acceptMode }: QuestItemProps) {
-    const [acceptLoading, setAcceptLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [acceptError, setAcceptError] = useState("");
-    const [removeLoading, setRemoveLoading] = useState(false);
-    const [completeLoading, setCompleteLoading] = useState(false);
-    const user = useSelector((state: ReturnType<typeof store.getState>) => state.user);
     const dispatch = useDispatch();
 
-    const accept = (challengeId: string, quest: Quest) => {
-        setAcceptLoading(true);
+    const handleAccept = useCallback(() => {
+        setLoading(true);
         setAcceptError("");
 
-        acceptQuest(challengeId, quest.id)
+        acceptQuest(challenge.id, quest.id)
             .then((newQuest) => {
-                // Add quest to challenge
                 dispatch(addQuestToChallenge(newQuest));
-                // Remove quest from all since it's selected
                 dispatch(removeQuest(newQuest.id));
-                setAcceptLoading(false);
+                setLoading(false);
                 setAcceptError("");
             })
             .catch((err) => {
                 setAcceptError(err.message);
-                setAcceptLoading(false);
+                setLoading(false);
             });
-    };
+    }, [dispatch, challenge.id, quest.id]);
 
-    const complete = (challenge: Challenge, quest: Quest) => {
+    const handleComplete = useCallback(() => {
         const coinCurrentNew = challenge.coinCurrent + quest.coin;
-
-        setCompleteLoading(true);
+        setLoading(true);
 
         completeQuest(challenge.id, quest.id, coinCurrentNew)
             .then(() => {
                 dispatch(completeQuestInChallenge({ questId: quest.id, coinCurrent: coinCurrentNew }));
-                setCompleteLoading(false);
+                setLoading(false);
             })
             .catch((err) => {
-                // TODO: Display error
                 console.log(err);
-                setCompleteLoading(false);
+                setLoading(false);
             });
-    };
+    }, [challenge.coinCurrent, challenge.id, quest.coin, quest.id, dispatch]);
 
-    const remove = (questId: string) => {
-        setRemoveLoading(true);
+    const handleRemove = useCallback(() => {
+        setLoading(true);
 
-        deleteQuest(questId).then(() => {
-            dispatch(removeQuest(questId));
-            setRemoveLoading(false);
+        deleteQuest(quest.id).then(() => {
+            dispatch(removeQuest(quest.id));
+            setLoading(false);
         });
-    };
-
-    const getDifficulityIcon = (difficulty: QuestDifficulties): string => {
-        switch (difficulty) {
-            case QuestDifficulties.Hard:
-                return flame;
-            case QuestDifficulties.Medium:
-                return seaWave;
-            case QuestDifficulties.Easy:
-                return wind;
-            default:
-                throw new Error("Unsupported quest difficulity...");
-        }
-    };
+    }, [dispatch, quest.id]);
 
     return (
         <Accordion.Item value={quest.id}>
@@ -142,74 +211,24 @@ export function QuestItem({ quest, challenge, acceptMode }: QuestItemProps) {
                 <Flex justify="space-between" gap={18} direction="column">
                     <Flex>
                         <BadgeWithImage
-                            imgSrc={getDifficulityIcon(quest.difficulty)}
+                            imgSrc={getDifficultyIcon(quest.difficulty)}
                             text={DifficultyTextMapping.get(quest.difficulty)!}
                             color="gray"
                             marginRight={8}
                         />
                         <BadgeWithImage imgSrc={coin} text={quest.coin} color="gray" marginRight={8} />
                         <BadgeWithImage imgSrc={REWARDS.get(quest.reward)!} text="1x" color="gray" marginRight={8} />
-                        {!acceptMode && quest.extraReward && (
-                            <HoverCard shadow="md">
-                                <HoverCard.Target>
-                                    <Center>
-                                        <BadgeWithImage imgSrc={EXTRA_REWARDS.get(quest.extraReward)!} text="1x" color={GOLD_COLOR} shine={true} />
-                                    </Center>
-                                </HoverCard.Target>
-                                <HoverCard.Dropdown>
-                                    <Image h={46} w={46} src={EXTRA_REWARDS.get(quest.extraReward)!} />
-                                </HoverCard.Dropdown>
-                            </HoverCard>
-                        )}
+                        {!acceptMode && quest.extraReward && <ExtraRewardHoverCard extraReward={quest.extraReward} />}
                     </Flex>
-                    {acceptMode ? (
-                        <Flex>
-                            {user.roles.includes(UserRoles.ADMINISTRATOR) && (
-                                <Tooltip label="Delete Quest" position="bottom" color="gray">
-                                    <ActionIcon
-                                        mr={8}
-                                        w="100%"
-                                        variant="light"
-                                        color="red"
-                                        aria-label="accept-challenge"
-                                        onClick={() => remove(quest.id)}
-                                        disabled={removeLoading}
-                                    >
-                                        <IconX size={18} />
-                                    </ActionIcon>
-                                </Tooltip>
-                            )}
-                            <Tooltip label="Accept Quest" position="bottom" color="gray">
-                                <ActionIcon
-                                    variant="light"
-                                    w="100%"
-                                    color={CategoryColorMapping.get(quest.category)}
-                                    disabled={acceptLoading}
-                                    aria-label="accept-challenge"
-                                    onClick={() => accept(challenge.id, quest)}
-                                >
-                                    <IconPlus size={18} />
-                                </ActionIcon>
-                            </Tooltip>
-                        </Flex>
-                    ) : (
-                        <>
-                            {!challenge.completedQuests.includes(quest.id) && (
-                                <Tooltip label="Complete Quest" position="bottom" color="gray">
-                                    <ActionIcon
-                                        variant="light"
-                                        w="100%"
-                                        color={CategoryColorMapping.get(quest.category)}
-                                        aria-label="complete-challenge"
-                                        disabled={completeLoading}
-                                        onClick={() => complete(challenge, quest)}
-                                    >
-                                        <IconCheck size={18} />
-                                    </ActionIcon>
-                                </Tooltip>
-                            )}
-                        </>
-                    )}
+                    <QuestActions
+                        quest={quest}
+                        challenge={challenge}
+                        acceptMode={acceptMode}
+                        handleAccept={handleAccept}
+                        handleComplete={handleComplete}
+                        handleRemove={handleRemove}
+                        loading={loading}
+                    />
                 </Flex>
             </Accordion.Panel>
         </Accordion.Item>
