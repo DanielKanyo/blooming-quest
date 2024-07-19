8ea5175d4bf17aa688a4ed550a7dad471eaf8b9d
DanielKanyo <danielkanyog@gmail.com>
2024-07-19 15:28:15 +0200

    Radus md for all elements 3

--- src/Components/QuestSkeletons.tsx
+++ src/Components/QuestSkeletons.tsx
@@ -8,7 +8,7 @@ export function QuestSkeletons() {
             {[...Array(NUM_OF_QUEST_SKELETONS).keys()].map((_item, key) => (
                 <Card key={key} mb={10} px="md" py={0} radius="md" style={{ width: "100%" }}>
                     <Group wrap="nowrap" py="sm">
-                        <Skeleton h={45} miw={45} w={45} animate={true} />
+                        <Skeleton h={45} miw={45} w={45} animate={true} radius="md" />
                         <div
                             style={{
                                 width: "100%",
@@ -18,8 +18,8 @@ export function QuestSkeletons() {
                                 justifyContent: "space-between",
                             }}
                         >
-                            <Skeleton h={22} w="100%" animate={true} />
-                            <Skeleton h={15} w="100%" animate={true} />
+                            <Skeleton h={22} w="100%" animate={true} radius="md" />
+                            <Skeleton h={15} w="100%" animate={true} radius="md" />
                         </div>
                     </Group>
                 </Card>
