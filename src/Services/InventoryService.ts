51b03a7f4f753be60b8bceef0efb2635b26baf8c
DanielKanyo <danielkanyog@gmail.com>
2024-07-23 15:15:08 +0200

    subtractItem added

--- src/Services/InventoryService.ts
+++ src/Services/InventoryService.ts
@@ -47,3 +47,23 @@ export const addItem = async (userId: string, itemId: string, timestamp: number,
 
     await updateDoc(userInventoryRef, { items: userInventory.items });
 };
+
+export const subtractItem = async (userId: string, itemId: string) => {
+    const userInventoryRef = doc(db, "inventories", userId);
+    const userInventorySnap = await getDoc(userInventoryRef);
+    const userInventory = userInventorySnap.data() as Inventory;
+
+    const item = userInventory.items[itemId];
+
+    if (item) {
+        if (item.quantity > 1) {
+            userInventory.items[itemId].quantity -= 1;
+        } else {
+            delete userInventory.items[itemId];
+        }
+
+        return await updateDoc(userInventoryRef, { items: userInventory.items });
+    } else {
+        console.error("Item not found in inventory...");
+    }
+};
