d405c8134772d90852522bc411b4d62724f44c27
danielkanyo <danielkanyo@debian.com>
2024-07-26 16:51:20 +0200

    Pond improvement, bugfix

--- src/Layouts/GardenArea/GardenArea.tsx
+++ src/Layouts/GardenArea/GardenArea.tsx
@@ -4,10 +4,11 @@ import { Card, Flex, SimpleGrid, Image, Group } from "@mantine/core";
 
 import bench from "../../Assets/Other/bench.png";
 import marsh from "../../Assets/Other/marsh.png";
+import reeds from "../../Assets/Other/reeds.png";
 import scarecrow from "../../Assets/Other/scarecrow.png";
 import shovel from "../../Assets/Other/shovel.png";
 import { Slot } from "../../Components/Slot/Slot";
-import { DEFAULT_GARDEN_SLOTS } from "../../Shared/GardenUtils";
+import { DEFAULT_GARDEN_SLOTS, GARDEN_FIELD_IDENTIFIER } from "../../Shared/GardenUtils";
 import { TARGET_AREAS } from "../../Shared/TargetAreas";
 import { Garden } from "../../Shared/Types/GardenType";
 import "./GardenArea.css";
@@ -36,7 +37,7 @@ const LeftSection = ({ garden, findItemInSlotBySlotId }: SectionProps) => {
                     gardenId={garden.gardenId}
                 />
             </Group>
-            <Group justify="center" gap="xl" mt={8} ml={-5}>
+            <Group justify="center" gap="xl" ml={-15}>
                 <Slot
                     slotId={DEFAULT_GARDEN_SLOTS.SL2}
                     itemId={findItemInSlotBySlotId(DEFAULT_GARDEN_SLOTS.SL2)}
@@ -54,11 +55,22 @@ const LeftSection = ({ garden, findItemInSlotBySlotId }: SectionProps) => {
                     gardenId={garden.gardenId}
                 />
             </Group>
-            <Image className="marsh" radius="md" h={300} w={300} src={marsh} />
-            <div className="marsh-slot">
+            <Image className="reeds" radius="md" h={70} w={70} src={reeds} />
+            <Image className="marsh" radius="md" h={400} w={400} src={marsh} />
+            <div className="marsh-slot1">
                 <Slot
                     slotId={DEFAULT_GARDEN_SLOTS.SL4}
                     itemId={findItemInSlotBySlotId(DEFAULT_GARDEN_SLOTS.SL4)}
+                    size={48}
+                    extraRewardSlot={true}
+                    target={TARGET_AREAS.GARDEN}
+                    gardenId={garden.gardenId}
+                />
+            </div>
+            <div className="marsh-slot2">
+                <Slot
+                    slotId={DEFAULT_GARDEN_SLOTS.SL5}
+                    itemId={findItemInSlotBySlotId(DEFAULT_GARDEN_SLOTS.SL5)}
                     size={48}
                     extraRewardSlot={true}
                     target={TARGET_AREAS.GARDEN}
@@ -74,6 +86,7 @@ const MiddleSection = ({ garden, findItemInSlotBySlotId }: SectionProps) => {
         <div className="middle-section">
             <SimpleGrid cols={6} spacing="xs" verticalSpacing="xs">
                 {Object.keys(garden.slots)
+                    .filter((slotId) => slotId.includes(GARDEN_FIELD_IDENTIFIER))
                     .sort()
                     .map((slotId) => {
                         return (
